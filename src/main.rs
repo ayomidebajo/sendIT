deec9de1feacaf24b1ce618b2370dadf05a21834
Ayomide <oluwashinabajo@gmail.com>
2022-02-24 01:11:19 +0100

    try usig ftp to create a file and open it too

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 use clap::{App, Arg};
 use std::net::TcpListener;
+use file_transfer::ftp;
+use std::fs::File;
 
 
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    // let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 // for stream in listener.incoming() {
 //     let stream = stream.unwrap();
 //     println!("Connection established!")
 // }
-match listener.accept() {
-    Ok((_socket, addr)) => println!("client: {:?}", addr),
-    Err(e) => println!("couldn't get client: {:?}", e),
-}
+
+let ftpva = ftp::FTP::new("127.0.0.1:8080", File::create("example1.txt").unwrap());
+    // Receives file from client
+    ftpva.recv();
+
+     let ftpse = ftp::FTP::new("127.0.0.1:8080", File::open("example.txt").unwrap());
+    // Sends File
+    ftpse.send();
+// match listener.accept() {
+//     Ok((_socket, addr)) => println!("client: {:?}", addr),
+//     Err(e) => println!("couldn't get client: {:?}", e),
+// }
 
 //     let app = App::new("sendit")
 // .about("An application for sending files")
