240a44c2b1ce60ed4bd5100076027c1a44068cf0
Ayomide <oluwashinabajo@gmail.com>
2022-03-06 09:00:02 +0100

    create server using tide and test it out with an example

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,33 @@
-use clap::{App, Arg};
-use std::net::TcpListener;
-use file_transfer::ftp;
-use std::fs::File;
+// use clap::{App, Arg};
+// use std::net::TcpListener;
+// use file_transfer::ftp;
+// use std::fs::File;
+use tide::Request;
+use tide::prelude::*;
 
 
 
-fn main() {
-    //FInd a way to spinup an ethernet sever of a wifi server
-    // let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-// for stream in listener.incoming() {
-//     let stream = stream.unwrap();
-//     println!("Connection established!")
-// }
-
-let ftpva = ftp::FTP::new("127.0.0.1:8080", File::create("example1.txt").unwrap());
-    // Receives file from client
-    ftpva.recv();
-
-     let ftpse = ftp::FTP::new("127.0.0.1:8080", File::open("example.txt").unwrap());
-    // Sends File
-    ftpse.send();
-// match listener.accept() {
-//     Ok((_socket, addr)) => println!("client: {:?}", addr),
-//     Err(e) => println!("couldn't get client: {:?}", e),
-// }
-
-//     let app = App::new("sendit")
-// .about("An application for sending files")
-// .arg(Arg::with_name("server").short('s').default_value()).get_matches();
-
-
-    // let domain = app.value_of("server").unwrap();
-    // println!("{:?}", domain);
+#[derive(Debug, Deserialize)]
+struct Animal {
+    name: String,
+    legs: u8
 }
+
+
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    let port: &str = "127.0.0.1:8080";
+    let mut app = tide::new();
+    app.at("/orders/shoes").post(order_shoes);
+    app.listen(port).await?;
+    println!("Server listening at port {:?}", &port);
+    Ok(())
+}
+
+
+async fn order_shoes(mut req: Request<()>) -> tide::Result {
+    let Animal { name, legs } = req.body_json().await?;
+    Ok(format!("Hello, {}! I've put in an order for {} shoes", name, legs).into())
+}
+
