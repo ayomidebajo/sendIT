19e0b9b7acf01de190db778e2f09990280c0c802
Ayomide <oluwashinabajo@gmail.com>
2022-03-23 23:37:12 +0100

    practice async

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,14 @@
 // use std::net::TcpListener;
 // use file_transfer::ftp;
 // use std::fs::File;
-use tide::Request;
+use futures::executor::block_on;
 use tide::prelude::*;
-
-
+use tide::Request;
 
 #[derive(Debug, Deserialize)]
 struct Animal {
     name: String,
-    legs: u8
+    legs: u8,
 }
 
 //todo read more on tcp
@@ -18,18 +17,29 @@ struct Animal {
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     let port: &str = "127.0.0.1:8080";
+    let learn = learn_song();
+    block_on(learn);
     let mut app = tide::new();
     tide::log::start();
-       app.at("/").get(|_| async { Ok("Hello, world!") });
+    app.at("/").get(|_| async { Ok("Hello, world!") });
     app.at("/orders/shoes").post(order_shoes);
     app.listen(port).await?;
     println!("Server listening at port {:?}", &port);
+
     Ok(())
 }
-
 
 async fn order_shoes(mut req: Request<()>) -> tide::Result {
     let Animal { name, legs } = req.body_json().await?;
     Ok(format!("Hello, {}! I've put in an order for {} shoes", name, legs).into())
 }
 
+async fn learn_song() {
+    println!("learn song")
+}
+async fn sing_song(song: &str) {
+    println!("Singing {}", song)
+}
+async fn dance() {
+    println!("Dancing to song")
+}
